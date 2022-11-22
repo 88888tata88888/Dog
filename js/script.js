7093bb1315d896a8b809901eb39421179fc7b1c2
88888tata88888 <tashakeks@gmail.com>
2022-11-22 11:39:16 +0100

    b n

---
+++ js/script.js
@@ -0,0 +1,52 @@
+//ACCORD
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+//MODALKA РАСЧЕТ
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[data-modal]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
+
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open2]'),
+    closeModalBtn: document.querySelector('[data-modal-close2]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+  
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+  ;
+
+  
+
+
+
+
+
